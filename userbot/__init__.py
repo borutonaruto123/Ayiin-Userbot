ac238ff6e56a7ad678c64e15d85231811ebb4a2b
borutonaruto123 <101137279+borutonaruto123@users.noreply.github.com>
2022-03-08 01:53:33 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -165,7 +165,7 @@ GITHUB_ACCESS_TOKEN = os.environ.get("GITHUB_ACCESS_TOKEN", None)
 
 # Custom (forked) repo URL for updater.
 UPSTREAM_REPO_URL = os.environ.get(
-    "UPSTREAM_REPO_URL", "https://github.com/AyiinXd/Ayiin-Userbot.git"
+    "UPSTREAM_REPO_URL", "https://github.com/borutonaruto123/Ayiin-Userbot.git"
 )
 
 # Custom Name Sticker Pack
